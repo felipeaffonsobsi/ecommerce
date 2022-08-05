8230cdc20ac2f0aca80313d2eaab504f7fc3c5ef
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-08-05 14:26:16 -0300

    Criando Configuração para 2ª App
    Configurando o segundo aplicativo no projeto (Template admin)

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+
+    public function __construct ($opts = array (), $tpl_dir = "/Github/ecommerce/views/admin/"){
+
+        parent::__construct($opts, $tpl_dir);
+    }
+
+}
+
+?>
