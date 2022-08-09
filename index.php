136c03efef2e5221a8df0d798fa98b7dd9d34715
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-08-09 01:22:35 -0300

    106. Admin Login - get_magic_quotes_gpc
    Feita toda a estrutura para a página de login e de conexão com o banco verificando se o usuário existe.
    
    Criado rotas post para realizar a consulta
    
    funcao logout
    
    trabalhando com sessao para verificar se o usuario está ou não logado.

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -19,9 +21,33 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLoginAdmin();
 	$page = new PageAdmin();
 	$page->setTpl("index");
 	
+});
+
+$app->get('/admin/login', function() {
+	User::verifyLogin();
+	$page = new PageAdmin([
+		"header"=> false,
+		"footer"=> false
+	]);
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /Github/ecommerce/admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){ 
+	User::logout();
+
+	header("Location: /Github/ecommerce/admin/login");
+	exit;
 });
 
 $app->run();
